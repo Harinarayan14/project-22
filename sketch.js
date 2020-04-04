b3539b900376de68cbdb950362392b311c0529d6
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2020-04-04 18:40:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,9 @@ var Engine = Matter.Engine,
  
 var engine;
 var world;
-var boxes = [];
+var boxes = [],gSlider;
  console.log(boxes);
 var ground;
-var gSlider;
  
 function setup() {
     createCanvas(400, 400);
@@ -23,7 +22,7 @@ function setup() {
     // A slider is already created for you here. This slider will dictate the gravity of the world
     gSlider = createSlider(0, 100, 50);
     gSlider.position(40, 365);
-    gSlider.input = map(engine.world.gravity, gSlider.min, gSlider.max, 0, 1,true);
+    
  
     ground = new Ground(200,390,400,20);
     // Create a ground rectangle that would hold all the boxes and add it to the world.
@@ -33,21 +32,29 @@ function setup() {
 function mousePressed() {
     if (mouseY < 350) {
         // Every time a mouse press occures create a new box.
-        box = new Box(mouseX,mouseY,Math.round(random(5,30)), Math.round(random(5,30)));
-        boxes.push(box);
-    box.display();
+         
+        boxes.push(new Box(mouseX,mouseY,random(5,30), random(5,30)));
         
     }
 }
  
 function draw() {
     // Draw all the elements including the slider that 
-Engine.update(engine);
-    background(0);
+
+    background(51);
+    
+    
     // This is the value of your gravity. You can optionally show it to the viewer.
     var fVal = gSlider.value();
+    text("Gravity" + fVal,50,15)
+    engine.world.gravity.y = map(fVal, 0, 10, 0, 1,true);
+    Engine.update(engine);
     // Use a for loop to show all the boxes
     ground.display();
+    for(var i = 0;i<boxes.length;i++){
+        boxes[i].display();
+    }
+    
     
 }
  
