1130a26875ed172927a9587e098a97355c917913
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2020-04-04 18:44:13 +0530

    Update box.js

--- box.js
+++ box.js
@@ -7,17 +7,25 @@ class Box
  
     // create your box using the function arguments
     this.body = Bodies.rectangle(x,y,w,h);
-      this.w = w; 
+    World.add(world, this.body);  
+    this.w = w; 
       this.h = h;
-      World.add(world, this.body);
+     
     // Create a show method which will draw the box every time it is called inside the draw method.
     // remember to push and pop.
 }
 display(){
-  push();
+ 
     var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
     rectMode(CENTER);
-  rect(pos.x, pos.y, this.w,this.h);
+    strokeWeight(1);
+    stroke(255);
+    fill(127);
+  rect(0, 0, this.w,this.h);
   pop();
   }
 }
